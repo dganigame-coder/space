cbcc60975583e4f33af97da541d8cb90b60f044f
dganigame-coder <dgani.game@gmail.com>
2026-04-25 23:53:12 +0300

    Create spacedata.js

---
+++ spacedata.js
@@ -0,0 +1,11 @@
+const spaceLibrary = [
+    { name: "כדור הארץ", type: "planet", color: 0x2266ff },
+    { name: "מאדים", type: "planet", color: 0xff4400 },
+    { name: "שבתאי", type: "planet", color: 0xdfbe9f, hasRings: true },
+    { name: "חור שחור", type: "blackhole", color: 0x000000 },
+    { name: "ערפילית אוריון", type: "nebula", color: 0xff00ff },
+    { name: "גלקסיית אנדרומדה", type: "galaxy", color: 0x00f2ff },
+    { name: "השמש", type: "sun", color: 0xffcc00 },
+    { name: "נפטון", type: "planet", color: 0x3355ff },
+    { name: "ענק אדום", type: "sun", color: 0xff3300 }
+];
