8f96a18a1c27a3bee684496347b5f446d58befcd
dganigame-coder <dgani.game@gmail.com>
2026-05-08 18:47:45 +0300

    Update mars.js

--- navisoftspace/planets/mars.js
+++ navisoftspace/planets/mars.js
@@ -4,16 +4,24 @@ export function createMars() {
     const loader = new THREE.TextureLoader();
     const group = new THREE.Group();
 
-    const mars = new THREE.MeshStandardMaterial({
-    map: loader.load('https://github.com/dganigame-coder/space/tree/master/navisoftspace/planets/texture/2k_mars.jpg'),
-    bumpMap: loader.load('https://github.com/dganigame-coder/space/tree/master/navisoftspace/planets/texture/8k_mars.jpg'),
-    bumpScale: 0.35,
-    roughness: 1,
-    metalness: 0
-    });
-    group.add(mars);
+    // 1. Create the Shape
+    const geometry = new THREE.SphereGeometry(400, 64, 64);
 
-    // Position Mars further out than the Moon
+    // 2. Create the Material (The Skin)
+    // NOTE: Use 'raw.githubusercontent.com' and remove '/tree/master/' 
+    const marsMaterial = new THREE.MeshStandardMaterial({
+        map: loader.load('https://raw.githubusercontent.com/dganigame-coder/space/master/navisoftspace/planets/texture/2k_mars.jpg'),
+        bumpMap: loader.load('https://raw.githubusercontent.com/dganigame-coder/space/master/navisoftspace/planets/texture/8k_mars.jpg'),
+        bumpScale: 0.35,
+        roughness: 1,
+        metalness: 0
+    });
+
+    // 3. Create the Mesh (The actual planet object)
+    const marsMesh = new THREE.Mesh(geometry, marsMaterial);
+    group.add(marsMesh);
+
+    // Position Mars
     group.position.set(-10000, 2000, -15000);
     group.userData = { 
         name: "MARS", 
@@ -21,8 +29,9 @@ export function createMars() {
         r: 400 
     };
 
+    // 4. Update the rotation on the MESH
     group.onUpdate = () => {
-        mars.rotation.y += 0.0006;
+        marsMesh.rotation.y += 0.0006;
     };
 
     return group;
