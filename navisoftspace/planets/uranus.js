e19b4c483e78f2d8df2b7b61dd30a53261808ffd
dganigame-coder <dgani.game@gmail.com>
2026-05-09 22:46:08 +0300

    Update uranus.js

--- navisoftspace/planets/uranus.js
+++ navisoftspace/planets/uranus.js
@@ -13,7 +13,8 @@ export function createUranus() {
         new THREE.SphereGeometry(1100, 64, 64),
         new THREE.MeshPhongMaterial({
             map: loader.load('https://cdn.jsdelivr.net/gh/dganigame-coder/space/navisoftspace/planets/texture/2k_uranus.jpg'),
-            shininess: 2
+            shininess: 2,
+            side: THREE.DoubleSide
         })
     );
     
