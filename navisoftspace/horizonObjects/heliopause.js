e46bc44884788eec793f669ab384ae63e45a448c
dganigame-coder <dgani.game@gmail.com>
2026-05-15 00:18:56 +0300

    Update heliopause.js

--- navisoftspace/horizonObjects/heliopause.js
+++ navisoftspace/horizonObjects/heliopause.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
 export function createHeliopause(scene, config) {
     const { radius, color, opacity } = config;
