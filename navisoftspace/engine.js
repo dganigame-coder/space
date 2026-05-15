f5188bda90379c23467026868b49fdb3b1b30843
dganigame-coder <dgani.game@gmail.com>
2026-05-15 20:54:14 +0300

    Update engine.js

--- navisoftspace/engine.js
+++ navisoftspace/engine.js
@@ -1,6 +1,6 @@
 /**10-05-26 22:41   **/
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { playHighFi } from 'audio';
+import { playHighFi, loadSoundLibrary  } from 'audio';
 /**
  * RESPONSIBILITY: 
  * 1. Initialize the WebGL Renderer
