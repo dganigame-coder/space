7c772c31eac949d407581d7091f7fe6722d1155f
dganigame-coder <dgani.game@gmail.com>
2026-07-28 22:44:43 +0300

    Update exoplanet import URL in exoplanetsystem.js

--- navisoftspace/horizonObjects/exoplanetsystem.js
+++ navisoftspace/horizonObjects/exoplanetsystem.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { createExoplanet } from 'https://cdn.jsdelivr.net/gh/dganigame-coder/space@5375f6d/navisoftspace/horizonObjects/exoplanet.js';
+import { createExoplanet } from 'https://cdn.jsdelivr.net/gh/dganigame-coder/space@b874aa7/navisoftspace/horizonObjects/exoplanet.js';
 
 export function createExoplanetSystem(scene, config = {}) {
     const system = new THREE.Group();
