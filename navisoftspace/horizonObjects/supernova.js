3930fc6dede30a3ba1b4363fed76b804b8c379b1
dganigame-coder <dgani.game@gmail.com>
2026-08-29 08:06:15 +0300

    Restore visibility for core and corona meshes
    Restore visibility for coreMesh and coronaMesh when stable.

--- navisoftspace/horizonObjects/supernova.js
+++ navisoftspace/horizonObjects/supernova.js
@@ -160,10 +160,8 @@ export function createSupernova(scene, config) {
             group.userData.state = 'STABLE';
             group.userData.timeToChange = (STABLE_END - age).toFixed(1) + 's';
             
-            //coreMesh.visible = true;
-            //coronaMesh.visible = true;
-            coreMesh.visible = false;   
-            coronaMesh.visible = false;
+            coreMesh.visible = true;
+            coronaMesh.visible = true;
             
             coreMesh.scale.setScalar(1.0);
             coronaMesh.scale.setScalar(1.08);
